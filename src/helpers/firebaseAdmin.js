201bdd31133aeafb27df476330abb4cb6cd5171a
Hasna Cristian <cristian.hasna@info.uaic.ro>
2019-05-28 12:07:55 +0300

    Parsing private key on development and production env

--- src/helpers/firebaseAdmin.js
+++ src/helpers/firebaseAdmin.js
@@ -2,11 +2,13 @@ const admin = require('firebase-admin');
 const dotenv = require('dotenv');
 dotenv.config();
 
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+
 const serviceAccount = {
 	type: process.env.TYPE,
 	project_id: process.env.PROJECT_ID,
 	private_key_id: process.env.PRIVATE_KEY_ID,
-	private_key: process.env.PRIVATE_KEY,
+	private_key: process.env.NODE_ENV === 'production' ? JSON.parse(process.env.PRIVATE_KEY) : process.env.PRIVATE_KEY,
 	client_email: process.env.CLIENT_EMAIL,
 	client_id: process.env.CLIENT_ID,
 	auth_uri: process.env.AUTH_URI,
