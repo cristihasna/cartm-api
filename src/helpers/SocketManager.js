7a1350381e26d986758989ed2d563162d47f801f
Hasna Cristian <cristian.hasna@info.uaic.ro>
2019-06-26 22:35:49 +0300

    Handle idle ws connections

--- src/helpers/SocketManager.js
+++ src/helpers/SocketManager.js
@@ -6,14 +6,28 @@ and dispatching messages to session participants
 module.exports = class SocketManager {
 	constructor() {
 		this.socketDict = {};
+		setInterval(() => {
+			for (let key in this.socketDict) {
+				if (!this.socketDict[key].isAlive) {
+					this.socketDict[key].terminate();
+					delete this.socketDict[key];
+				}
+				this.socketDict[key].ping(null, false, true);
+			}
+		}, 5000);
 	}
 
 	add(userEmail, socket) {
-        this.socketDict[userEmail] = socket;
+		this.socketDict[userEmail] = socket;
+		this.socketDict[userEmail].isAlive = true;
+		this.socketDict[userEmail].on('pong', () => {
+			this.socketDict[userEmail].isAlive = true;
+		})
         console.log(`adding ${userEmail} to the party`);
 	}
 
 	remove(userEmail) {
+		this.socketDict[userEmail].terminate();
         delete this.socketDict[userEmail];
         console.log(`removing ${userEmail} from the party`);
 	}
